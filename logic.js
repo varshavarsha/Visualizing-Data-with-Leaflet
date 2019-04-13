6b48576b8f4d3aac4a8e43ffc3a0e06b13b43404
varshavarsha <44000780+varshavarsha@users.noreply.github.com>
2019-04-13 12:54:51 -0400

    Update logic.js

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@
 var earthquakeUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
   "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
-var platesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+var platesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
 // Perform a GET request to the query URL
 d3.json(earthquakeUrl, function (earthquakedata) {
